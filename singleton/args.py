db633094bab66e0cba9f0d24efaad1d2bc748849
Rahmanism <s.mostafa.rahmani@gmail.com>
2021-11-10 21:09:06 +0330

    args and kwargs

---
+++ singleton/args.py
@@ -0,0 +1,12 @@
+def test_args(arg1, *args, **kargs):
+  print('First arg: ', arg1)
+  for a in args:
+    print('Argument: ', a)
+  if kargs is not None:
+    for ka, kav in kargs.items():
+      print(ka, ': ', kav)
+  else:
+    print('no kargs')
+
+test_args('f', 1,5,'ff','ss', x=5 )
+test_args('r',x=5)
